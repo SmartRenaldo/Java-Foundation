3fe27595867d72431357b7eb1b04b0930bb9bcff
SmartRenaldo <102466979+SmartRenaldo@users.noreply.github.com>
2022-03-27 22:15:24 +1030

    Add files via upload

---
+++ Test1/System of Numeration/src/Practice01/Test1.java
@@ -0,0 +1,15 @@
+package Practice01;
+
+public class Test1 {
+    public static void main(String[] args) {
+        //Decimal: default
+        //Binary: starts with 0b
+        //Octal: starts with 0
+        //Hexadecimal: starts with 0x
+        //Output value: decimal
+        System.out.println(10);
+        System.out.println(0b10);
+        System.out.println(010);
+        System.out.println(0x10);
+    }
+}
---
+++ Test1/System of Numeration/src/Practice02/Byte_test.java
@@ -0,0 +1,8 @@
+package Practice02;
+
+public class Byte_test {
+    public static void main(String[] args) {
+        byte b = (byte) 130;
+        System.out.println(b);
+    }
+}
