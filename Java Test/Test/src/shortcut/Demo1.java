f7dfe433f79cf2cda3e00f8a7446f50401d21beb
SmartRenaldo <102466979+SmartRenaldo@users.noreply.github.com>
2022-03-27 22:09:53 +1030

    Add files via upload

---
+++ Java Test/Test/src/shortcut/Demo1.java
@@ -0,0 +1,13 @@
+package shortcut;
+
+public class Demo1 {
+    /*
+        psvm + enter : public static void main(String[] args) {}
+        sout + enter : System.out.println();
+        alt + 1 : open/close project directory
+        alt + 4 : open/close console
+     */
+    public static void main(String[] args) {
+        System.out.println("NEW WORLD");
+    }
+}
---
+++ Java Test/Test/src/shortcut/Demo2.java
@@ -0,0 +1,20 @@
+package shortcut;
+
+public class Demo2 {
+    /*
+
+        Ctrl + alt + L              formatting code
+        Alt + Enter                 automatical correction
+        Ctrl + D                    copy to nextline
+        Ctrl + X                    cut the line
+        ctrl + /                    Add single-line comments in bulk
+        ctrl + shift + /            Add multi-line comments in bulk
+        ctrl + shift + ↑            Move up one line
+        ctrl + shift + ↓            Move down one line
+        shift + Enter               Create next line
+    */
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+    }
+}
